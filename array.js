35f61a88b410617d305d83696934a5a78097d48d
ahmadalwan <madajadeh2023@gmail.com>
2025-12-29 10:08:49 +0700

    Menambahkan file array.js dan menambahkan isi datanya

--- array.js
+++ array.js
@@ -3,11 +3,18 @@
  // - Menggunakan tanda []
  const fruits = ["Apel", "Anggur", "Jeruk"];
  const animals = ["Kucing", "Singa", "Elang"];
+ fruits.push("Durian"); // push = menambahkan data dari paling belakang
+ animals.unshift("Kambing"); // unshift = menambahkan data dari paling depan
+
+ // - Menggunakan looping forEach
+ // - forEach berfungsi untuk melooping data di array JS
+ fruits.forEach((fruit) => console.log(fruit));
+ animals.forEach((animal) => console.log(animal));
 
  // --- Cara manggilnya --- //
  // - index dimulai dari 0
  // - sebut nama variblenya lalu index keberapa
- console.log(fruits[0]);
- console.log(animals[1]);
+//  console.log(fruits[0]);
+//  console.log(animals[1]);
 
 
