f6f75098b8d1a0bdfbc910e48390f38578d6b058
ahmadalwan <madajadeh2023@gmail.com>
2025-12-29 10:03:23 +0700

    Menambahkan file array.js dan menambahkan isi datanya

---
+++ array.js
@@ -0,0 +1,13 @@
+ // === MEMBUAT ARRAY YANG SEDERHANA MELALUI JAVASCRIPT === //
+
+ // - Menggunakan tanda []
+ const fruits = ["Apel", "Anggur", "Jeruk"];
+ const animals = ["Kucing", "Singa", "Elang"];
+
+ // --- Cara manggilnya --- //
+ // - index dimulai dari 0
+ // - sebut nama variblenya lalu index keberapa
+ console.log(fruits[0]);
+ console.log(animals[1]);
+
+
