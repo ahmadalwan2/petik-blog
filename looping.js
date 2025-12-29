95d3c1edcfef53ceb0c7fa0143ef2747968de472
ahmadalwan <madajadeh2023@gmail.com>
2025-12-29 09:44:27 +0700

    Menambahkan data di file looping.js

--- looping.js
+++ looping.js
@@ -5,7 +5,22 @@
  // nilai akhir/kondisi akhir
  // increment/decrement 
 
+ // --- ini contoh increment
+ // ciri khasnya itu menggunakkan ++
  for (let i = 0; i < 10; i++) {
-        console.log(`Looping ke-${i}`);
-            
+        console.log(`Looping ke-${i}`);      
+ }
+
+ // --- ini contoh decrement 
+ // ciri khasnya itu menggunakan --
+  for (let i = 10; i > 0; i--) {
+        console.log(`Looping ke-${i}`);    
+ }
+
+ // - Looping while: 
+ // menjalankan perintah selama kondisi terpenuhi
+ let nilai = 0;
+ while (nilai < 5) {
+    console.log(nilai);
+    nilai++;
  }
