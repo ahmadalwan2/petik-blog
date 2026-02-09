eb3c866fcffc54848ec5c77490fb9591aa0f978d
ahmadalwan <madajadeh2023@gmail.com>
2026-02-09 11:01:03 +0700

    ubah tag a

--- petik-blog/src/components/Mynavbar/MyNavbar.jsx
+++ petik-blog/src/components/Mynavbar/MyNavbar.jsx
@@ -1,31 +1,33 @@
 import "./MyNavbar.css";
 import { NavLink, Link } from "react-router-dom";
 import { Container, Nav, Navbar } from "react-bootstrap";
+
 const MyNavbar = () => {
   return (
-    <div>
-      <Navbar className="Nav bg-primary">
-        <Container>
+    <Navbar className="Nav bg-primary" expand="lg">
+      <Container>
+        {/* Brand langsung jadi Link */}
         <Navbar.Brand as={Link} to="/">
           Petik Blog
         </Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link>
-              <NavLink to={"/"}>Home</NavLink>
-            </Nav.Link>
-            <Nav.Link>
-              <NavLink to={"/posts"}>Posts</NavLink>
-            </Nav.Link>
-            <Nav.Link>
-              <NavLink to={"/news"}>News</NavLink>
-            </Nav.Link>
-            <Nav.Link>
-              <NavLink to={"/about"}>About</NavLink>
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </div>
+
+        <Nav className="me-auto">
+          {/* Nav.Link langsung jadi NavLink */}
+          <Nav.Link as={NavLink} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/posts">
+            Posts
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/news">
+            News
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/about">
+            About
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
